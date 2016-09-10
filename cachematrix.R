35625e089cd439be8d430800eabe8a4bd863020f
palro <srividya817@gmail.com>
2016-09-10 22:02:21 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## setx creates a function to receive the matrix and cache it in variable x and 
+## initialize an empty inverted matrix visible globally
+## getx is a function that simply gets the data or matrix stored in x
+## setinv stores the invertedmatrix into inv
+## getinv gets the inverted matrix set earlier in setinv
 
-## Write a short comment describing this function
+## makeCacheMatrix contains functions needed to cache a matrix and store& return the inverted matrix in a new variable 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                
+                inv <- NULL 
+                setx <- function(x1) {
+                  x <<- x1
+                  inv <<- NULL
+                }
+                getx <- function()x
+                
+                setinv <- function(invmat) inv <<- invmat
+                getinv <- function()inv
+                
+                list(setx = setx, getx = getx, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Function to invert the cached matrix if it is not already inverted in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
+  
+         y <- x$getx()
+         inv <- x$getinv() 
+         if(is.null(inv)) {
+            
+            message("getting cached matrix")
+            inv <- solve(y)
+            x$setinv(inv)
+            inv
+          }
+          
         ## Return a matrix that is the inverse of 'x'
 }
